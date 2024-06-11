308a6b579e846cb19b408d4ae3a29b7bcf6cce78
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-06-11 13:57:46 +0900

    feat:JpaMemberRepository

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,6 +5,7 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +24,7 @@ public class MemberService {
     /**
      * 회원가입
      */
+    @Transactional
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원x
         validateDuplicateMember(member);
